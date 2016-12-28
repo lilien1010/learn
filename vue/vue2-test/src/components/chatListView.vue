1f1575f51d30dbd38c47064e6c85adbc5337bbd1
lilien1010 <lilien1010@gmail.com>
2016-12-28 19:29:24 +0800

    addv vue test

---
+++ vue/vue2-test/src/components/chatListView.vue
@@ -0,0 +1,45 @@
+<div class="phone-viewport">
+  <md-list>
+
+
+    <md-subheader>Contacts</md-subheader>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="assets/avatar-2.jpg" alt="People">
+      </md-avatar>
+
+      <span>Abbey Christansen</span>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon class="md-primary">chat_bubble</md-icon>
+      </md-button>
+    </md-list-item>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="assets/avatar-2.jpg" alt="People">
+      </md-avatar>
+
+      <span>Alex Nelson</span>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon class="md-primary">chat_bubble</md-icon>
+      </md-button>
+    </md-list-item>
+
+    <md-list-item>
+      <md-avatar>
+        <img src="assets/avatar-2.jpg" alt="People">
+      </md-avatar>
+
+      <span>Mary Johnson</span>
+
+      <md-button class="md-icon-button md-list-action">
+        <md-icon>chat_bubble</md-icon>
+      </md-button>
+    </md-list-item>
+  </md-list>
+</div>
+
+ 
